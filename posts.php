f23d577ef94ca146f2e075f8ca9ed9b8cc943609
jluukvg <j.luukvg@gmail.com>
2016-11-15 16:34:37 -0600

    Files for creating shown posts.

---
+++ posts.php
@@ -0,0 +1,84 @@
+<?php
+    include_once "common/base.php";
+    include_once "common/header.php";
+?>
+
+
+    <p>Put something here.</p>
+
+    <div class="container">
+    <div class="row">
+        <div class="[ col-xs-12 col-sm-offset-1 col-sm-5 ]">
+            <div class="[ panel panel-default ] panel-google-plus">
+                <div class="dropdown">
+                    <span class="dropdown-toggle" type="button" data-toggle="dropdown">
+                        <span class="[ glyphicon glyphicon-chevron-down ]"></span>
+                    </span>
+                    <ul class="dropdown-menu" role="menu">
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+                        <li role="presentation" class="divider"></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+                    </ul>
+                </div>
+                <div class="panel-heading">
+                    <img class="[ img-circle pull-left ]" src="https://lh3.googleusercontent.com/-CxXg7_7ylq4/AAAAAAAAAAI/AAAAAAAAAQ8/LhCIKQC5Aq4/s46-c-k-no/photo.jpg" alt="Mouse0270" />
+                    <h3>Robert McIntosh</h3>
+                    <h5><span>Shared publicly</span> - <span>Jun 27, 2014</span> </h5>
+                </div>
+                <div class="panel-body">
+                    <p>Do people born in 2000 get to choose if they are Generation Y or Generation Z? How do you decide what generation you want to belong to?</p>
+                </div>
+                <div class="post_website">
+                                            <div class="close" title="Remove"></div>
+                                            <div class="inner">
+
+                                                <div class="details">
+                                                    <div class="info">
+                                                        <div class="image">
+                                                        <img src="http://lorempixel.com/400/200/" alt="Smiley face" height="42" width="42"></div>
+                                                        <div class="title"> <p>Title</p></div>
+                                                        <div class="description">
+                                                        <p>This is a description.</p></div>
+                                                        <div class="url" title="url"> </div>
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+                                        </div>
+                <div class="panel-footer">
+                    <button type="button" class="[ btn btn-default ]">+1</button>
+                    <button type="button" class="[ btn btn-default ]">
+                        <span class="[ glyphicon glyphicon-share-alt ]"></span>
+                    </button>
+                    <div class="input-placeholder">Add a comment...</div>
+                </div>
+                <div class="panel-google-plus-comment">
+                    <img class="img-circle" src="https://lh3.googleusercontent.com/uFp_tsTJboUY7kue5XAsGA=s46" alt="User Image" />
+                    <div class="panel-google-plus-textarea">
+                        <textarea rows="4"></textarea>
+                        <button type="submit" class="[ btn btn-success disabled ]">Post comment</button>
+                        <button type="reset" class="[ btn btn-default ]">Cancel</button>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <script src="post_window.js">
+
+
+            </script>
+
+
+
+
+
+
+    </body>
+
+    <?php include_once "common/footer.php"; ?>
