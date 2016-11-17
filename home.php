c93fd0ae890e81a52f6672ac6e41f588dacd61fb
jluukvg <j.luukvg@gmail.com>
2016-11-17 16:26:46 -0600

    Addition to last commit
    This should have been added to the last commit.

--- home.php
+++ home.php
@@ -25,29 +25,33 @@
                                         <textarea placeholder="What would you like to share?" name="post" id="post"></textarea>
                                         <ul>
 
-                                            <li><select class="selectpicker" data-width="100%" name="topic" title="Topic">
-                                            <option value="1">Movies</option>
-                                            <option value="2">Music</option>
-                                            <option value="3">Events</option>
-                                            <option value="4">Food</option>
-                                            <option value="5">Restaurants</option>
-                                            <option value="6">Sports</option>
-                                            <option value="7">Books</option>
-                                            <option value="8">Fashion</option>
-                                            <option value="9">Wanderlust</option>
-                                            <option value="10">Funny</option>
-                                            <option value="11">Interesting</option>
-                                            <option value="12">Lifestyle</option>
-                                            </select></li>
+                                            <li>
+                                                <select class="selectpicker" data-width="100%" name="topic" title="Topic">
+                                                    <option value="1">Movies</option>
+                                                    <option value="2">Music</option>
+                                                    <option value="3">Events</option>
+                                                    <option value="4">Food</option>
+                                                    <option value="5">Restaurants</option>
+                                                    <option value="6">Sports</option>
+                                                    <option value="7">Books</option>
+                                                    <option value="8">Fashion</option>
+                                                    <option value="9">Wanderlust</option>
+                                                    <option value="10">Funny</option>
+                                                    <option value="11">Interesting</option>
+                                                    <option value="12">Lifestyle</option>
+                                                </select>
+                                            </li>
                                         </ul>
                                         <ul>
 
-                                            <li><select class="selectpicker" data-width="100%" name="media" title="Media">
-                                            <option value="1">Video</option>
-                                            <option value="2">Blog</option>
-                                            <option value="3">Podcast</option>
-                                            <option value="4">Article</option>
-                                            </select></li>
+                                            <li>
+                                                <select class="selectpicker" data-width="100%" name="media" title="Media">
+                                                    <option value="1">Video</option>
+                                                    <option value="2">Blog</option>
+                                                    <option value="3">Podcast</option>
+                                                    <option value="4">Article</option>
+                                                </select>
+                                            </li>
                                         </ul>
                                         <button type="submit" class="btn btn-success green" data-toggle="tooltip" data-placement="bottom" title="Hooray!"><i class="fa fa-share"></i> Share</button>
                                         <div class="liveurl">
@@ -82,23 +86,21 @@
 
 
             <script src="jquery.liveurl.js">
-
-
             </script>
             <script>
                 var curImages = new Array();
 
                 $('textarea').liveUrl({
                     // para empezar a mostrar el "loading"
-                    loadStart: function() {
+                    loadStart: function () {
                         $('.liveurl-loader').show();
                     },
                     // para dejar de mostrar el "loading"
-                    loadEnd: function() {
+                    loadEnd: function () {
                         $('.liveurl-loader').hide();
                     },
                     // si se encuentra una URL válida, empieza el proceso principal
-                    success: function(data) {
+                    success: function (data) {
                         var output = $('.liveurl');
                         output.find('.title').text(data.title);
                         output.find('.description').text(data.description);
@@ -106,7 +108,7 @@
                         output.find('.image').empty();
 
                         // esto es sólo para cerrar el liveURL picándole a la X
-                        output.find('.close').one('click', function() {
+                        output.find('.close').one('click', function () {
                             var liveUrl = $(this).parent();
                             liveUrl.hide('fast');
                             liveUrl.find('.video').html('').hide();
@@ -124,7 +126,7 @@
 
                     },
                     // sin esta función no se puede(n) mostrar la(s) imagen(es)
-                    addImage: function(image) {
+                    addImage: function (image) {
                         var output = $('.liveurl');
                         var jqImage = $(image);
                         jqImage.attr('alt', 'Preview');
@@ -156,86 +158,102 @@
                         output.find('.thumbnail .max').text(curImages.length);
                     }
                 });
-
             </script>
-        </div><br>
+        </div>
+        <br>
 
         <div class="container">
             <div class="row">
                 <div class="col-sm-4">
                     <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
-                        <div class="panel-body"><img src="images/movies.jpg" class="img-responsive" style="width:100%;" alt="Image">
-                            <h1 style="text-align:center;">MOVIES</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
-                        <div class="panel-body"><img src="images/music.jpg" class="img-responsive" style="width:100%" alt="Image">
-                            <h1 style="text-align:center;">MUSIC</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
-                        <div class="panel-body"><img src="images/events.jpg" class="img-responsive" style="width:100%" alt="Image">
-                            <h1 style="text-align:center;">EVENTS</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><br>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <div>
-                        <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;"><a href=posts.php?topic=4>
-                            <div class="panel-body"><img src="images/food.jpg" class="img-responsive" style="width:100%" alt="Image">
-                                <h1 style="text-align:center;">FOOD</h1>
-                            </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;"><a href=posts.php?topic=5>
-                        <div class="panel-body"><img src="images/restaurants.jpg" class="img-responsive" style="width:100%" alt="Image">
-                            <h1 style="text-align:center;">RESTAURANTS</h1>
+                        <a href=posts.php?topic=1>
+                            <div class="panel-body"><img src="images/movies.jpg" class="img-responsive" style="width:100%;" alt="Image">
+                                <h1 style="text-align:center;">MOVIES</h1>
                         </a>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
-                        <div class="panel-body"><img src="images/sports.jpg" class="img-responsive" style="width:100%" alt="Image">
-                            <h1 style="text-align:center;">SPORTS</h1>
+                        <a href=posts.php?topic=2>
+                            <div class="panel-body"><img src="images/music.jpg" class="img-responsive" style="width:100%" alt="Image">
+                                <h1 style="text-align:center;">MUSIC</h1>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
+                        <a href=posts.php?topic=3>
+                            <div class="panel-body"><img src="images/events.jpg" class="img-responsive" style="width:100%" alt="Image">
+                                <h1 style="text-align:center;">EVENTS</h1>
+                        </a>
                         </div>
                     </div>
                 </div>
             </div>
-        </div><br>
+        </div>
+        <br>
 
         <div class="container">
             <div class="row">
                 <div class="col-sm-4">
                     <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
-                        <div class="panel-body"><img src="images/books.jpg" class="img-responsive" style="width:100%;" alt="Image">
-                            <h1 style="text-align:center;">BOOKS</h1>
+                        <a href=posts.php?topic=4>
+                            <div class="panel-body"><img src="images/food.jpg" class="img-responsive" style="width:100%" alt="Image">
+                                <h1 style="text-align:center;">FOOD</h1>
+                        </a>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
-                        <div class="panel-body"><img src="images/fashion.jpg" class="img-responsive" style="width:100%" alt="Image">
-                            <h1 style="text-align:center;">FASHION</h1>
+                        <a href=posts.php?topic=5>
+                            <div class="panel-body"><img src="images/restaurants.jpg" class="img-responsive" style="width:100%" alt="Image">
+                                <h1 style="text-align:center;">RESTAURANTS</h1>
+                        </a>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
-                        <div class="panel-body"><img src="images/wanderlust.jpg" class="img-responsive" style="width:100%" alt="Image">
-                            <h1 style="text-align:center;">WANDERLUST</h1>
+                        <a href=posts.php?topic=6>
+                            <div class="panel-body"><img src="images/sports.jpg" class="img-responsive" style="width:100%" alt="Image">
+                                <h1 style="text-align:center;">SPORTS</h1>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
+                        <a href=posts.php?topic=7>
+                            <div class="panel-body"><img src="images/books.jpg" class="img-responsive" style="width:100%;" alt="Image">
+                                <h1 style="text-align:center;">BOOKS</h1>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
+                        <a href=posts.php?topic=8>
+                            <div class="panel-body"><img src="images/fashion.jpg" class="img-responsive" style="width:100%" alt="Image">
+                                <h1 style="text-align:center;">FASHION</h1>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div style="background-color:#F5F5F5; border-radius: 28px; box-shadow: 2px 2px 7px #666666;">
+                        <a href=posts.php?topic=9>
+                            <div class="panel-body"><img src="images/wanderlust.jpg" class="img-responsive" style="width:100%" alt="Image">
+                                <h1 style="text-align:center;">WANDERLUST</h1>
+                        </a>
                         </div>
                     </div>
                 </div>
@@ -243,7 +261,8 @@
             <?php
             print_r($_SESSION);
             ?>
-        </div><br>
+        </div>
+        <br>
 
 
         <script type="text/javascript" src="toggle_script.js"></script>
